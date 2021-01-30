1b91393e831161af1a0070db14f60f22fdbaceb8
Michaela Guzmická <mgmichaelaguzmicka@gmail.com>
2021-01-30 14:40:46 +0100

    add additional headers

--- api/post/create.php
+++ api/post/create.php
@@ -2,6 +2,9 @@
 // Headers
 header('Access-Control-Allow-Origin: *');
 header('Contect-Type: application/json');
+header('Acess-Control-Allow-Methods: POST');
+header('Acess-Control-Allow-Headers: Acess-Control-Allow-Headers,Contect-Type
+Acess-Control-Allow-Methods, Authorization, X-Requested-With');
 
 include_once '../../config/database.php';
 include_once '../../models/post.php';
